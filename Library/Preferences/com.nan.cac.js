f9525d85b8c15d9ea71530c9585d27597c3b6d61
zhaonan <35675770+invalidunit@users.noreply.github.com>
2024-06-25 15:50:50 +0800

    Update com.nan.cac.js

--- Library/Preferences/com.nan.cac.js
+++ Library/Preferences/com.nan.cac.js
@@ -18,8 +18,8 @@
   ],
   "enable": 1,
   "message": {
-    "cache": "app_name\\n当前最新版本：app_version\\n已缓存到本地等待下一次对比",
-    "del": "app_bundle\\n可能已下架，最后出现版本为：\\napp_version",
-    "up": "app_name\\n发现变更：\\n版本：app_version"
+    "cache": "app_name\\n当前最新版本：\\napp_version\\n版本 id 为：\\napp_version_id\\n已缓存到本地等待下一次对比",
+    "del": "app_bundle\\n可能已下架，已连续 10 次获取信息失败，最后出现版本为：\\napp_version",
+    "up": "app_name\\n发现版本变更：\\napp_version\\n新版本 id 为：\\napp_version_id"
   }
 }
