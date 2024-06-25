7a5f1db8bc66d41cea34572e51ee044891eed6cc
zhaonan <35675770+invalidunit@users.noreply.github.com>
2024-06-25 14:56:47 +0800

    Create com.nan.cac.js

---
+++ Library/Preferences/com.nan.cac.js
@@ -0,0 +1,25 @@
+{
+  "app": [
+    {
+      "enable": 1,
+      "name": "QQ",
+      "url": "https://apps.apple.com/cn/app/id444934666"
+    },
+    {
+      "enable": 1,
+      "name": "微信",
+      "url": "https://apps.apple.com/cn/app/id414478124"
+    },
+    {
+      "enable": 1,
+      "name": "抖音",
+      "url": "https://apps.apple.com/cn/app/id1142110895"
+    }
+  ],
+  "enable": 1,
+  "message": {
+    "cache": "app_name\\n当前最新版本：app_version\\n已缓存到本地等待下一次对比",
+    "del": "app_bundle\\n可能已下架，最后出现版本为：\\napp_version",
+    "up": "app_name\\n发现变更：\\n版本：app_version"
+  }
+}
