6a4db8cf4761d3663a36f1e1f7b7462fda7698d2
GitHub Action <action@github.com>
2024-06-28 03:25:40 +0000

    save caches

--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -3,8 +3,8 @@
   "bundle": "com.tencent.mqq",
   "id": 444934666,
   "name": "QQ",
-  "version": "9.0.65",
-  "version_id": 866704804,
+  "version": "9.0.70",
+  "version_id": 867014164,
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/14/9a/86/149a869b-15ea-10cb-1f7e-3b0f565ad8d0/AppIcon-1-0-1x_U007emarketing-0-7-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
