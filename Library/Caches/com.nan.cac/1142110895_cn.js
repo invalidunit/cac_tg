5315c0b6c363ae7c8cde96971315085de5ed46f7
GitHub Action <action@github.com>
2024-10-08 08:32:14 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -3,8 +3,8 @@
   "bundle": "com.ss.iphone.ugc.Aweme",
   "id": 1142110895,
   "name": "抖音",
-  "version": "30.3.0",
-  "version_id": 866847750,
+  "version": "31.6.0",
+  "version_id": 867110786,
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/96/bc/18/96bc182d-e579-b1e2-81a8-f55a1805ce2a/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
