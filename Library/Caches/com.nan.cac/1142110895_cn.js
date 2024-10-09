458a22196d5e01652db5d8c0c00efe095c697bf9
GitHub Action <action@github.com>
2024-10-09 01:49:59 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -6,5 +6,5 @@
   "version": "31.6.0",
   "version_id": 867110786,
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/69/90/e7/6990e7e3-e12a-28a9-d541-7b16abeeab14/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/96/bc/18/96bc182d-e579-b1e2-81a8-f55a1805ce2a/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
