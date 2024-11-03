9e59604a079b8f71be7dba30ea1d5bffa4c339f7
GitHub Action <action@github.com>
2024-11-03 03:00:49 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -5,6 +5,5 @@
   "name": "抖音",
   "version": "31.9.0",
   "version_id": 870019663,
-  "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/68/f0/22/68f02259-8568-7a4f-cfb8-0640ad07dd68/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "price": "免费"
 }
--- Library/Caches/com.nan.cac/414478124_cn.js
+++ Library/Caches/com.nan.cac/414478124_cn.js
@@ -5,6 +5,5 @@
   "name": "微信",
   "version": "8.0.53",
   "version_id": 869236155,
-  "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b9/29/bc/b929bce5-8c10-412c-1220-eae72d66a1af/AppIcon-0-0-1x_U007emarketing-0-4-0-0-85-220.png/100x100bb.jpg"
+  "price": "免费"
 }
